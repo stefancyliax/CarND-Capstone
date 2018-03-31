4f5c4a779a23f5063728d950fda01430d227c70c
Unknown <stcyliax@gmail.com>
2018-03-31 17:48:33 +0200

    Small fix for first iteration when waypoint_tree isn't filed yet

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -49,7 +49,7 @@ class WaypointUpdater(object):
     def loop(self):
         rate = rospy.Rate(UPDATE_RATE)
         while not rospy.is_shutdown():
-            if self.pose and self.base_lane:
+            if self.pose and self.base_lane and self.waypoint_tree:
                 self.publish_waypoints()
             rate.sleep
 
