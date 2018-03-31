9f46c554b6df304788075ba66d696fb8cc79ad32
Paul Michalik <paul.michalik@outlook.com>
2018-04-01 01:12:11 +0200

    Add implementation impl.TLClassifier + unit tests

--- ros/src/tl_detector/light_classification/impl/tl_classifier.py
+++ ros/src/tl_detector/light_classification/impl/tl_classifier.py
@@ -2,19 +2,20 @@ import tensorflow as tf
 import numpy as np
 
 class TLClassifier(object):
-    s_min_score_thresh = .5
-    s_category_index = {
+    c_min_score_thresh = .5
+    c_category_index = {
         1 : 'green',
         2 : 'red',
         3 : 'yellow',
         4 : 'none'
     }
 
-    def _init_graph(path_to_graph):
+    @staticmethod
+    def init_graph(path_to_graph):
         graph = tf.Graph()
         config = tf.ConfigProto()
         config.gpu_options.allow_growth = True
-
+        # Load graph from ProtoBuf file into default instance which is set to "graph". This is a weird idiom but well, who cares...
         with graph.as_default():
             od_graph_def = tf.GraphDef()
             with tf.gfile.GFile(path_to_graph, 'rb') as fid:
@@ -23,35 +24,35 @@ class TLClassifier(object):
 
         return graph, config
 
-    def _init_tensors(self):
-        # Definite input and output Tensors for detection_graph
-        self.image_tensor = self.graph.get_tensor_by_name('image_tensor:0')
-        self.detection_boxes = self.graph.get_tensor_by_name('detection_boxes:0')
-        self.detection_scores = self.graph.get_tensor_by_name('detection_scores:0')
-        self.detection_classes = self.graph.get_tensor_by_name('detection_classes:0')
-        self.num_detections = self.graph.get_tensor_by_name('num_detections:0')
+    @staticmethod
+    def init_tensors(graph):
+        # Retrieve input and output tensors from the graph
+        return graph.get_tensor_by_name('image_tensor:0'), [ 
+            graph.get_tensor_by_name('detection_boxes:0'), 
+            graph.get_tensor_by_name('detection_scores:0'),
+            graph.get_tensor_by_name('detection_classes:0'),
+            graph.get_tensor_by_name('num_detections:0')
+        ]
 
-    def __init__(self, *args, **kwargs):
-        path_to_graph = kwargs['path_to_graph']
-        self.graph, self.config = _init_graph(path_to_graph)
+    def __init__(self, path_to_graph, *args, **kwargs):
+        self.graph, self.config = TLClassifier.init_graph(path_to_graph)
+        self.image_tensor, self.other_tensors = TLClassifier.init_tensors(self.graph)
         self.session = tf.Session(graph=self.graph, config=self.config)
-
         return super().__init__(*args, **kwargs)
 
     def get_class_name(self, idx):
-        return self.s_category_index[idx]
+        return self.c_category_index[idx]
 
     def get_classification(self, image):
         exp_image = np.expand_dims(image, axis=0)
         
-        (boxes, scores, classes, num) = self.session.run([self.detection_boxes,                 self.detection_scores, 
-                self.detection_classes, 
-                self.num_detections], feed_dict={ self.image_tensor: exp_image})
+        (boxes, scores, classes, num) = self.session.run(self.other_tensors, feed_dict={ self.image_tensor: exp_image} )
 
         boxes, scores, classes, num = np.squeeze(boxes), np.squeeze(scores), np.squeeze(classes).astype(np.int32), num
 
+        class_name = "none"
         for i in range(boxes.shape[0]):
-            if scores is None or scores[i] > self.s_min_score_thresh:
+            if scores is None or scores[i] > self.c_min_score_thresh:
                 class_name = self.get_class_name(classes[i])
 
         return class_name
--- ros/src/tl_detector/test/test_tl_classifier.py
+++ ros/src/tl_detector/test/test_tl_classifier.py
@@ -1,31 +1,62 @@
 import os, sys
 sys.path.append(os.path.abspath(os.path.join('..')))
 
-import light_classification as lc
+import light_classification.impl.tl_classifier as lc_impl
+
 import unittest
 
+def get_models_dir():
+    return os.path.join(os.path.dirname(__file__), "..", "light_classification", "frozen_models")
+
+def get_model_path(model_name):
+    return os.path.join(get_models_dir(), model_name, "frozen_inference_graph.pb")
+
 class TestTLClassifier(unittest.TestCase):
+    c_models = {
+        "sim_inception": get_model_path("frozen_sim_inception-5000"),
+        "real_inception": get_model_path("frozen_real_inception-5000"),
+        "sim_mobile": get_model_path("frozen_sim_mobile-5000"),
+        "real_mobile": get_model_path("frozen_real_mobile-5000"),
+    }
+
     def setUp(self):
         return super().setUp()
 
     def tearDown(self):
         return super().tearDown()
 
-    def test_can_instantiate(self):
-        cl = lc.impl.TLClassifier('../light_classification/frozen_models/frozen_sim_inception-5000/frozen_inference_graph.pb')
-        self.assertTrue(cl != None)
-
     def test_can_load_sim_inception_model(self):
-        return
+        cl = lc_impl.TLClassifier(self.c_models["sim_inception"])
+        self.assertTrue(cl != None)
+        self.assertTrue(cl.graph != None)
+        self.assertTrue(cl.config != None)
+        self.assertTrue(cl.image_tensor != None)
+        self.assertTrue(cl.other_tensors != None and len(cl.other_tensors) == 4)
 
     def test_can_load_real_inception_model(self):
-        return
+        cl = lc_impl.TLClassifier(self.c_models["real_inception"])
+        self.assertTrue(cl != None)
+        self.assertTrue(cl.graph != None)
+        self.assertTrue(cl.config != None)
+        self.assertTrue(cl.image_tensor != None)
+        self.assertTrue(cl.other_tensors != None and len(cl.other_tensors) == 4)
 
     def test_can_load_sim_mobile_model(self):
-        return
+        cl = lc_impl.TLClassifier(self.c_models["sim_mobile"])
+        self.assertTrue(cl != None)
+        self.assertTrue(cl.graph != None)
+        self.assertTrue(cl.config != None)
+        self.assertTrue(cl.image_tensor != None)
+        self.assertTrue(cl.other_tensors != None and len(cl.other_tensors) == 4)
+
 
     def test_can_load_real_mobile_model(self):
-        return
+        cl = lc_impl.TLClassifier(self.c_models["real_mobile"])
+        self.assertTrue(cl != None)
+        self.assertTrue(cl.graph != None)
+        self.assertTrue(cl.config != None)
+        self.assertTrue(cl.image_tensor != None)
+        self.assertTrue(cl.other_tensors != None and len(cl.other_tensors) == 4)
 
 if __name__ == '__main__':
     unittest.main()
