0d7d2daed129fe0cdfcf71d4cb00fce9810d5548
Patrick Schalast <patrick.schalast@gmail.com>
2018-03-31 12:51:24 +0200

    Changes to initialization of waypoint updater

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -30,22 +30,18 @@ class WaypointUpdater(object):
     def __init__(self):
         rospy.init_node('waypoint_updater')
 
+        self.pose = None
+        self.base_lane = None
+        self.waypoints_2d = None
+        self.waypoint_tree = None
+        self.stopline_wp_idx = None
+
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
         rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
         rospy.Subscriber('/obstacle_waypoint', Int32, self.obstacle_cb)
 
-        # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
-
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
-
-        # TODO: Add other member variables you need below
-        
-        self.pose = None
-        self.base_lane = None
-        self.waypoints_2d = None
-        self.waypoints_tree = None
-        self.stopline_wp_idx = None
         
         self.loop()
 
@@ -60,7 +56,7 @@ class WaypointUpdater(object):
     def get_closest_waypoint_idx(self):
         x = self.pose.pose.position.x
         y = self.pose.pose.position.y
-        closest_idx = self.waypoints_tree.query([x,y], 1)[1]
+        closest_idx = self.waypoint_tree.query([x,y], 1)[1]
 
         # check if closest waypoint is ahead or behind the vehicle
         closest_coord = self.waypoints_2d[closest_idx]
@@ -107,23 +103,15 @@ class WaypointUpdater(object):
             temp.append(p)
         
         return temp
-
-
-
-"""     def publish_final_wp(self, closest_idx):
-        lane = Lane()
-        lane.header = self.base_waypoints.header
-        lane.waypoints = self.base_waypoints.waypoints[closest_idx : closest_idx + LOOKAHEAD_WPS]
-        self.final_waypoints_pub.publish(lane) """
-
+    
     def pose_cb(self, msg):
         self.pose = msg
-
+    
     def waypoints_cb(self, waypoints):
         self.base_lane = waypoints
         if not self.waypoints_2d:
             self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
-            self.waypoints_tree = KDTree(self.waypoints_2d)
+            self.waypoint_tree = KDTree(self.waypoints_2d)
 
     def traffic_cb(self, msg):
         self.stopline_wp_idx = msg.data
